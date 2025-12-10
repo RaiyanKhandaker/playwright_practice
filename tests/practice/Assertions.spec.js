a7efd6625fa555fc79bbaaf76b69a797a624fa8e
RaiyanKhandaker <rrkhandaker@gmail.com>
2025-12-10 17:05:59 +0600

    second commit

--- tests/practice/Assertions.spec.js
+++ tests/practice/Assertions.spec.js
@@ -4,5 +4,15 @@ test ('AssertionsTest', async ({page})=>{
 
     //open URL
     await page.goto('https://demo.nopcommerce.com/');
+
+    //page have URL
+    await expect(page).toHaveURL('https://demo.nopcommerce.com/');
+
+    await expect(page).toHaveTitle('nopCommerce demo store. Home page title');
     
+    const logoElement = await page.locator("//div[@class='header-logo']");
+    expect(logoElement).toBeVisible();
+
+    const searchBox = await page.locator("#small-searchterms");
+    await expect(searchBox).toBeEnabled();
 })
